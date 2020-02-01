c22450f1c52588a22ccc6b5fa4683076f5120c6c
Daniel Gonzalez <chiki4089@gmail.com>
2020-02-01 13:41:51 +0100

    Kojima.

---
+++ Assets/Scripts/Components/AAA/WebcamToRenderTexture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcamToRenderTexture : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer _renderer = null;
+
+    private void Awake()
+    {
+        if (_renderer == null)
+        {
+             Debug.LogError("[WebcamToRenderTexture.Awake] ERROR: Serializable _renderer not set");
+            return;
+        }
+        Application.RequestUserAuthorization(UserAuthorization.WebCam);
+    }
+
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture(1280,720,30);
+        _renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
