eecd07e29d6d27a92ec678cc7ea494b888e6dba1
victor martinez simon <victormartinezsimon@gmail.com>
2020-02-01 03:50:00 +0100

    add a scene for the litterature. reorder some assets

--- Assets/Scripts/ChangeMouse.cs
+++ Assets/Scripts/ChangeMouse.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Collider2D))]
 [RequireComponent(typeof(SpriteOutline))]
 public class ChangeMouse : MonoBehaviour
 {
---
+++ Assets/Scripts/Managers/Scenes/LiteratureManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiteratureManager : MonoBehaviour
+{
+    public GameObject _personaje;
+    public List<GameObject> _pistas;
+    private List<bool> _pistaConocida;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pistaConocida = new List<bool>();
+        for(int i = 0;i < _pistas.Count; ++i)
+        {
+            _pistaConocida.Add(false);
+        }
+    }
+
+    public void PistaConocida(int index)
+    {
+        _pistaConocida[index] = true;
+        Debug.Log("ya conocemos la pista =>" + index);
+    }
+
+    
+}
