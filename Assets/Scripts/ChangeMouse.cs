379ade10fe14442f77475b8e982c656d33e44320
victor martinez simon <victormartinezsimon@gmail.com>
2020-02-01 01:21:03 +0100

    add scene test to test the mousechanged

---
+++ Assets/Scripts/ChangeMouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ChangeMouse : MonoBehaviour
+{
+    public Texture2D _prevSprite;
+    public Texture2D _sprite;
+
+    private void Start()
+    {
+        Cursor.SetCursor(_prevSprite, Vector2.zero, CursorMode.Auto);
+    }
+
+    private void OnMouseEnter()
+    {
+        Cursor.SetCursor(_sprite, Vector2.zero, CursorMode.Auto);
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(_prevSprite, Vector2.zero, CursorMode.Auto);
+    }
+}
+    
