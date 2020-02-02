2d36e4841f5324a71cd757fe7dbd46aa95b55cc8
morax <m.moracuesta@gmail.com>
2020-02-02 13:39:23 +0100

    GLichCOlage

--- Assets/Scripts/Components/Glichs/GlichComponent.cs
+++ Assets/Scripts/Components/Glichs/GlichComponent.cs
@@ -3,13 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(Animator))]
 public class GlichComponent : IGlich
 {
     public Sprite m_glichImage;
     public Sprite m_fixedImage;
     private SpriteRenderer m_spriteComponent;
-    private Animator m_animator;
     [Range(1,10)]
     public float levelOfGlichToFix = 5;
 
@@ -17,29 +15,16 @@ public class GlichComponent : IGlich
     void Start()
     {
         m_spriteComponent = GetComponent<SpriteRenderer>();
-        m_animator = GetComponent<Animator>();
         m_spriteComponent.sprite = m_glichImage;
-        StartCoroutine(changeAnimator());
-    }
-
-    IEnumerator changeAnimator()
-    {
-        while (true)
-        {
-
-            if (m_spriteComponent.sprite == m_glichImage)
-            {
-                m_animator.speed = UnityEngine.Random.Range(0.0f, 3.0f);
-            }
-            yield return new WaitForSeconds(2.5f);
-        }
-        
     }
 
     public override void glich(float level)
     {
         m_spriteComponent.sprite = m_glichImage;
         bool fix = level < levelOfGlichToFix;
-        m_animator.SetBool("fixed", fix);
+        if (fix)
+            m_spriteComponent.sprite = m_fixedImage;
+        else
+            m_spriteComponent.sprite = m_glichImage;
     }
 }
