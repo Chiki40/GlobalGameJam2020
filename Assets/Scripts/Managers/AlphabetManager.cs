797647ddea5076b04ead9b8b208b33136eefb1fc
victor martinez simon <victormartinezsimon@gmail.com>
2020-02-02 03:14:05 +0100

    changes to the alphabet and the first scene

---
+++ Assets/Scripts/Managers/AlphabetManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphabetManager : MonoBehaviour
+{
+    private List<GameObject> alphabets;
+    private int currentAlphabet;
+    public bool isClickable = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int totalHijos = transform.childCount;
+        alphabets = new List<GameObject>();
+
+        for(int i = 0; i< totalHijos; ++i)
+        {
+            alphabets.Add(transform.GetChild(i).gameObject);
+        }
+
+        foreach (var a in alphabets)
+        {
+            a.SetActive(false);
+        }
+        alphabets[0].SetActive(true);
+        currentAlphabet = 0;
+    }
+
+    void IncreaseAlphabet()
+    {
+        if (!isClickable) return;
+        foreach (var a in alphabets)
+        {
+            a.SetActive(false);
+        }
+        currentAlphabet = (currentAlphabet + 1) % alphabets.Count;
+        alphabets[currentAlphabet].SetActive(true);
+    }
+
+    public void OnMouseUp()
+    {
+        IncreaseAlphabet();
+    }
+
+    void resetAlphabet()
+    {
+        foreach (var a in alphabets)
+        {
+            a.SetActive(false);
+        }
+        currentAlphabet = 0;
+        alphabets[currentAlphabet].SetActive(true);
+    }
+}
